7a79a6e446f2d70cd34f516a9da0ff84ea9f523c
soportetecnolift <soporte@tecnolift.es>
2026-03-11 06:19:22 -0500

    Ajuste en el mensaje se bienvenida

--- validar.js
+++ validar.js
@@ -44,15 +44,20 @@ async function createGroup(stringSessionn = null , nombre= "", descripcion="", d
         );
 
         const mensaje = await client.sendMessage(chat, {
-        message:"🏗 OBRA "+detailsMessage.obra+
-        " \n \n 👤Cliente: "+detailsMessage.cliente+
-        " \n 📱Teléfono: "+detailsMessage.telefono+
-        " \n 📌Dirección obra: "+detailsMessage.dirObra+
-        " \n ⚙Equipo: "+detailsMessage.equipo+
-        " \n #⃣Número de serie: "+detailsMessage.numSerie+
-        " \n 👨‍💻Comercial: "+detailsMessage.comercial+
-        " \n 📅Fecha prevista instalación: "+detailsMessage.fecha_prevista+
-        " \n \n 🔗Enlace de Invitación del Grupo:" + invite.link
+        message:"👷‍♂️ Bienvenidos al grupo de la obra"+
+        " \n \n** Grupo - "+nombre+"**"+
+        "\n \n Este grupo se utilizará para coordinar y compartir información importante sobre el desarrollo de la obra: avances, programación de trabajos, documentación y novedades."+
+        "\n \n Por favor mantener la comunicación relacionada únicamente con la obra y compartir información clara para una mejor coordinación."+
+        "\n \n **¡Buen trabajo para todos!**"+
+        "\n \n 🏗 OBRA "+detailsMessage.obra+
+        " \n \n 👤**Cliente:** "+detailsMessage.cliente+
+        " \n 📱**Teléfono:** "+detailsMessage.telefono+
+        " \n 📌**Dirección obra:** "+detailsMessage.dirObra+
+        " \n ⚙**Equipo:** "+detailsMessage.equipo+
+        " \n #⃣**Número de serie:** "+detailsMessage.numSerie+
+        " \n 👨‍💻**Comercial:** "+detailsMessage.comercial+
+        " \n 📅**Fecha prevista instalación:** "+detailsMessage.fecha_prevista+
+        " \n \n 🔗**Enlace de Invitación del Grupo:**" + invite.link+" "
         })
         await client.invoke(
             new Api.messages.UpdatePinnedMessage({
